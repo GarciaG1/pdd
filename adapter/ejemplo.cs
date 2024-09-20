835723801546f5d6f1be4ab3211053b31a4c4819
Gabriel Garcia <54416706+GarciaG1@users.noreply.github.com>
2024-09-20 14:42:44 -0700

    tarea2

--- adapter/ejemplo.cs
+++ adapter/ejemplo.cs
@@ -1,107 +1,27 @@
-using System;
-
-// Producto final: un Pedido (construido por el builder)
-public class Pedido
+public interface ITarget
 {
-    public string Hamburguesa { get; set; }
-    public string Bebida { get; set; }
-    public bool ConQueso { get; set; }
-    public bool ConLechuga { get; set; }
-    public bool ConTomate { get; set; }
+    void Request();
+}
 
-    public override string ToString()
+public class Adaptee
+{
+    public void SpecificRequest()
     {
-        return "Hamburguesa: " + Hamburguesa + ", Queso: " + ConQueso + ", Lechuga: " + ConLechuga +
-               ", Tomate: " + ConTomate + ", Bebida: " + Bebida;
+        Console.WriteLine("SpecificRequest in Adaptee");
     }
 }
 
-// Interfaz Builder para construir pedidos
-public interface IPedidoBuilder
+public class Adapter : ITarget
 {
-    void AgregarHamburguesa(string tipo);
-    void AgregarBebida(string bebida);
-    void ConQueso();
-    void ConLechuga();
-    void ConTomate();
-    Pedido ObtenerPedido();
-}
+    private readonly Adaptee adaptee;
 
-// Implementación del Builder para construir un pedido paso a paso
-public class PedidoBuilder : IPedidoBuilder
-{
-    private Pedido _pedido = new Pedido();
-
-    public void AgregarHamburguesa(string tipo)
+    public Adapter(Adaptee adaptee)
     {
-        _pedido.Hamburguesa = tipo;
+        this.adaptee = adaptee;
     }
 
-    public void AgregarBebida(string bebida)
+    public void Request()
     {
-        _pedido.Bebida = bebida;
-    }
-
-    public void ConQueso()
-    {
-        _pedido.ConQueso = true;
-    }
-
-    public void ConLechuga()
-    {
-        _pedido.ConLechuga = true;
-    }
-
-    public void ConTomate()
-    {
-        _pedido.ConTomate = true;
-    }
-
-    public Pedido ObtenerPedido()
-    {
-        return _pedido;
-    }
-}
-
-// Clase principal que contiene el método Main()
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        // Crear el director
-        Director director = new Director();
-
-        // Crear un nuevo builder de pedidos
-        PedidoBuilder builder = new PedidoBuilder();
-
-        // Construir un pedido completo
-        director.ConstruirPedidoCompleto(builder);
-        Pedido pedidoCompleto = builder.ObtenerPedido();
-        Console.WriteLine("Pedido Completo: " + pedidoCompleto);
-
-        // Construir un pedido simple
-        PedidoBuilder builderSimple = new PedidoBuilder();
-        director.ConstruirPedidoSimple(builderSimple);
-        Pedido pedidoSimple = builderSimple.ObtenerPedido();
-        Console.WriteLine("Pedido Simple: " + pedidoSimple);
-    }
-}
-
-// Director: controla el proceso de construcción
-public class Director
-{
-    public void ConstruirPedidoCompleto(IPedidoBuilder builder)
-    {
-        builder.AgregarHamburguesa("Hamburguesa de Pollo");
-        builder.ConQueso();
-        builder.ConLechuga();
-        builder.ConTomate();
-        builder.AgregarBebida("Coca Cola");
-    }
-
-    public void ConstruirPedidoSimple(IPedidoBuilder builder)
-    {
-        builder.AgregarHamburguesa("Hamburguesa Simple");
-        builder.AgregarBebida("Agua");
+        adaptee.SpecificRequest();
     }
 }
--- builder/ejemplo.cs
+++ builder/ejemplo.cs
@@ -1,30 +1,107 @@
-public class Product
+using System;
+
+// Producto final: un Pedido (construido por el builder)
+public class Pedido
 {
-    public string PartA { get; set; }
-    public string PartB { get; set; }
+    public string Hamburguesa { get; set; }
+    public string Bebida { get; set; }
+    public bool ConQueso { get; set; }
+    public bool ConLechuga { get; set; }
+    public bool ConTomate { get; set; }
+
+    public override string ToString()
+    {
+        return "Hamburguesa: " + Hamburguesa + ", Queso: " + ConQueso + ", Lechuga: " + ConLechuga +
+               ", Tomate: " + ConTomate + ", Bebida: " + Bebida;
+    }
 }
 
-public interface IBuilder
+// Interfaz Builder para construir pedidos
+public interface IPedidoBuilder
 {
-    void BuildPartA();
-    void BuildPartB();
-    Product GetResult();
+    void AgregarHamburguesa(string tipo);
+    void AgregarBebida(string bebida);
+    void ConQueso();
+    void ConLechuga();
+    void ConTomate();
+    Pedido ObtenerPedido();
 }
 
-public class ConcreteBuilder : IBuilder
+// Implementación del Builder para construir un pedido paso a paso
+public class PedidoBuilder : IPedidoBuilder
 {
-    private Product product = new Product();
+    private Pedido _pedido = new Pedido();
 
-    public void BuildPartA() => product.PartA = "PartA";
-    public void BuildPartB() => product.PartB = "PartB";
-    public Product GetResult() => product;
+    public void AgregarHamburguesa(string tipo)
+    {
+        _pedido.Hamburguesa = tipo;
+    }
+
+    public void AgregarBebida(string bebida)
+    {
+        _pedido.Bebida = bebida;
+    }
+
+    public void ConQueso()
+    {
+        _pedido.ConQueso = true;
+    }
+
+    public void ConLechuga()
+    {
+        _pedido.ConLechuga = true;
+    }
+
+    public void ConTomate()
+    {
+        _pedido.ConTomate = true;
+    }
+
+    public Pedido ObtenerPedido()
+    {
+        return _pedido;
+    }
 }
 
+// Clase principal que contiene el método Main()
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Crear el director
+        Director director = new Director();
+
+        // Crear un nuevo builder de pedidos
+        PedidoBuilder builder = new PedidoBuilder();
+
+        // Construir un pedido completo
+        director.ConstruirPedidoCompleto(builder);
+        Pedido pedidoCompleto = builder.ObtenerPedido();
+        Console.WriteLine("Pedido Completo: " + pedidoCompleto);
+
+        // Construir un pedido simple
+        PedidoBuilder builderSimple = new PedidoBuilder();
+        director.ConstruirPedidoSimple(builderSimple);
+        Pedido pedidoSimple = builderSimple.ObtenerPedido();
+        Console.WriteLine("Pedido Simple: " + pedidoSimple);
+    }
+}
+
+// Director: controla el proceso de construcción
 public class Director
 {
-    public void Construct(IBuilder builder)
+    public void ConstruirPedidoCompleto(IPedidoBuilder builder)
     {
-        builder.BuildPartA();
-        builder.BuildPartB();
+        builder.AgregarHamburguesa("Hamburguesa de Pollo");
+        builder.ConQueso();
+        builder.ConLechuga();
+        builder.ConTomate();
+        builder.AgregarBebida("Coca Cola");
+    }
+
+    public void ConstruirPedidoSimple(IPedidoBuilder builder)
+    {
+        builder.AgregarHamburguesa("Hamburguesa Simple");
+        builder.AgregarBebida("Agua");
     }
 }
